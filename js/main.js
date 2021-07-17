5bbf7adaa726bbd484bb609e178b3dec36b443da
Florian Rupprecht <floruppr@gmail.com>
2021-07-17 12:10:46 +0200

    small improvements

--- js/main.js
+++ js/main.js
@@ -12,8 +12,8 @@ function check(bibtex) {
             const cc = c.charCodeAt(0);
             if (cc > 126) {
                 errors.push({
-                    line_index: line_index,
-                    char_index: char_index,
+                    index: { line: line_index, char: char_index },
+                    end_index: { line: line_index, char: char_index + 1 },
                     text: "Weird symbol '" + c + "'",
                 });
             }
@@ -23,8 +23,8 @@ function check(bibtex) {
         let m;
         while (m = re.exec(line)) {
             errors.push({
-                line_index: line_index,
-                char_index: m.index,
+                index: { line: line_index, char: m.index },
+                end_index: { line: line_index, char: m.index + 8 },
                 text: "Weird escape '" + m[0] + "'",
             });
         }
@@ -32,25 +32,45 @@ function check(bibtex) {
     return errors;
 }
 // setup
+function jumpToLine(editor, i) {
+    var t = editor.charCoords({ line: i, ch: 0 }, "local").top;
+    var middleHeight = editor.getScrollerElement().offsetHeight / 2;
+    editor.scrollTo(null, t - middleHeight - 5);
+}
 function main() {
     const txt_bibtex = document.getElementById('bibinput');
-    var myCodeMirror = CodeMirror.fromTextArea(txt_bibtex, {
+    const editor = CodeMirror.fromTextArea(txt_bibtex, {
         lineNumbers: true,
-        mode: "javascript"
+        mode: "stex"
     });
     const btn_check = document.getElementById('check');
+    const btn_clear = document.getElementById('btn-clear');
     const elem_output = document.getElementById('erroutput');
     btn_check.onclick = () => {
         elem_output.innerHTML = '';
-        const errors = check(myCodeMirror.getValue());
+        const errors = check(editor.getValue());
+        if (errors.length == 0) {
+            const newDiv = document.createElement("a");
+            const newContent = document.createTextNode("No errors found.");
+            newDiv.appendChild(newContent);
+            elem_output === null || elem_output === void 0 ? void 0 : elem_output.appendChild(newDiv);
+            return;
+        }
         for (let e of errors) {
-            const newDiv = document.createElement("code");
+            const newDiv = document.createElement("a");
             newDiv.classList.add('error-entry');
-            const newContent = document.createTextNode(`${e.line_index + 1}, ${e.char_index}: ${e.text}`);
+            const newContent = document.createTextNode(`${e.index.line + 1}, ${e.index.char}: ${e.text}`);
+            newDiv.onclick = () => {
+                jumpToLine(editor, e.index.line + 2);
+            };
             newDiv.appendChild(newContent);
             elem_output === null || elem_output === void 0 ? void 0 : elem_output.appendChild(newDiv);
             elem_output === null || elem_output === void 0 ? void 0 : elem_output.appendChild(document.createElement('br'));
+            editor.markText({ line: e.index.line, ch: e.index.char }, e.end_index ? { line: e.end_index.line, ch: e.end_index.char } : { line: e.index.line, ch: e.index.char + 1 }, { className: "styled-background" });
         }
+    };
+    btn_clear.onclick = () => {
+        editor.setValue("");
     };
 }
 window.onload = function () {
--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,14 @@ const ESCAPE_SYMBOLS = {
     '\u00FC': '\"{u}',
 }
 
+interface TextPos {
+    line: number,
+    char: number,
+}
+
 interface BibError {
-    line_index: number,
-    char_index: number,
+    index: TextPos,
+    end_index?: TextPos,
     text: string,
 }
 
@@ -22,8 +27,8 @@ function check(bibtex: string): BibError[] {
             const cc = c.charCodeAt(0);
             if (cc > 126) {
                 errors.push({
-                    line_index: line_index,
-                    char_index: char_index,
+                    index: { line: line_index, char: char_index },
+                    end_index: { line: line_index, char: char_index+1 },
                     text: "Weird symbol '" + c + "'",
                 })
             }
@@ -37,8 +42,8 @@ function check(bibtex: string): BibError[] {
         let m: RegExpExecArray | null;
         while (m = re.exec(line)) {
             errors.push({
-                line_index: line_index,
-                char_index: m.index,
+                index: { line: line_index, char: m.index },
+                end_index: { line: line_index, char: m.index+8 },
                 text: "Weird escape '" + m[0] + "'",
             })
 
@@ -49,32 +54,59 @@ function check(bibtex: string): BibError[] {
 
 }
 
-
 // setup
+
+function jumpToLine(editor: CodeMirror.Editor, i: number): void { 
+    var t = editor.charCoords({line: i, ch: 0}, "local").top; 
+    var middleHeight = editor.getScrollerElement().offsetHeight / 2; 
+    editor.scrollTo(null, t - middleHeight - 5); 
+} 
 
 function main() {
 
     const txt_bibtex = document.getElementById('bibinput') as HTMLTextAreaElement;
 
-    var myCodeMirror = CodeMirror.fromTextArea(txt_bibtex, {
+    const editor = CodeMirror.fromTextArea(txt_bibtex, {
         lineNumbers: true,
-        mode: "javascript"
+        mode: "stex"
     });
 
     const btn_check = document.getElementById('check') as HTMLButtonElement;
+    const btn_clear = document.getElementById('btn-clear') as HTMLButtonElement;
     const elem_output = document.getElementById('erroutput') as HTMLElement;
 
     btn_check.onclick = () => {
         elem_output.innerHTML = '';
-        const errors = check(myCodeMirror.getValue());
+        const errors = check(editor.getValue());
+
+        if (errors.length == 0) {
+            const newDiv = document.createElement("a");
+            const newContent = document.createTextNode("No errors found.");
+            newDiv.appendChild(newContent);
+            elem_output?.appendChild(newDiv);
+            return;
+        }
+
         for (let e of errors) {
-            const newDiv = document.createElement("code");
+            const newDiv = document.createElement("a");
             newDiv.classList.add('error-entry')
-            const newContent = document.createTextNode(`${e.line_index + 1}, ${e.char_index}: ${e.text}`);
+            const newContent = document.createTextNode(`${e.index.line + 1}, ${e.index.char}: ${e.text}`);
+            newDiv.onclick = () => {
+                jumpToLine(editor, e.index.line+2);
+            }
             newDiv.appendChild(newContent);
             elem_output?.appendChild(newDiv);
             elem_output?.appendChild(document.createElement('br'));
+            
+            editor.markText(
+                {line: e.index.line, ch: e.index.char}, 
+                e.end_index ? {line: e.end_index.line, ch: e.end_index.char} : {line: e.index.line, ch: e.index.char+1}, 
+                {className: "styled-background"});
         }
+    }
+
+    btn_clear.onclick = () => {
+        editor.setValue("");
     }
 }
 
